7a7807de7f0b9bdf53bc5eecdb472a2428043320
HarisGGEZ <haris.sauban@gmail.com>
2025-05-09 13:27:08 +0200

    saker

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import pool from "./db.js"
 
 const app = express();
 const port = 3000;
@@ -10,7 +11,7 @@ const db = new pg.Pool({
 user: "postgres",
   host: "localhost",
   database: "ORM",
-  password: "123",
+  password: "orm123.",
   port: 5432,
 });
 
@@ -26,45 +27,49 @@ app.listen(port, () => {
 
 app.get("/", async (req, res) => {
    //const cart = await db.query("SELECT * FROM cart");
-    const items = await db.query("SELECT * FROM items ORDER BY id")
+    const items = await pool.query("SELECT * FROM items ORDER BY id")
     
     
-    const cart = await db.query("SELECT items.ID, items.item, cart.amount, items.price, items.price * cart.amount AS totalprice FROM items INNER JOIN cart ON items.item = cart.item ORDER BY id");
+    const cart = await pool.query("SELECT items.ID, items.item, cart.amount, items.price, items.price * cart.amount AS totalprice FROM items INNER JOIN cart ON items.item = cart.item ORDER BY id");
     res.render("index.ejs", {cart: cart.rows, items: items.rows} );
 })
 
+app.get("/cart", async (req, res) =>{
+    res.render("kundvagn.ejs")
+
+})
 
 app.post("/add", async (req, res) =>{
     const item = req.body.item_name
     
     
     
-    const currentItemExists = await db.query("SELECT EXISTS (SELECT item FROM cart WHERE item = '" + item + "')");
+    const currentItemExists = await pool.query("SELECT EXISTS (SELECT item FROM cart WHERE item = '" + item + "')");
     if (currentItemExists.rows[0].exists == false){
-        await db.query("INSERT INTO cart (item) VALUES ($1)", [item]);
+        await pool.query("INSERT INTO cart (item) VALUES ($1)", [item]);
 
     }
     else {
-        await db.query("UPDATE cart SET amount = amount + 1 WHERE item = '" + item + "'")
+        await pool.query("UPDATE cart SET amount = amount + 1 WHERE item = '" + item + "'")
 
     }
-    await db.query("UPDATE items SET amount = amount + 1 WHERE item = '" + item + "'")
+    await pool.query("UPDATE items SET amount = amount + 1 WHERE item = '" + item + "'")
 
     res.redirect("/");
 })
 
 app.post("/remove", async (req, res) =>{
     const item = req.body.item_name
-    const currentAmount = await db.query("SELECT amount FROM cart WHERE item = '" + item + "'");
+    const currentAmount = await pool.query("SELECT amount FROM cart WHERE item = '" + item + "'");
     try{
     if (currentAmount.rows[0].amount > 1){
-        await db.query("UPDATE cart SET amount = amount - 1 WHERE item = '" + item + "'")
-        await db.query("UPDATE items SET amount = amount - 1 WHERE item = '" + item + "'")
+        await pool.query("UPDATE cart SET amount = amount - 1 WHERE item = '" + item + "'")
+        await pool.query("UPDATE items SET amount = amount - 1 WHERE item = '" + item + "'")
 
     }
     else {
-        await db.query("DELETE FROM cart WHERE item = '" + item + "'")
-        await db.query("UPDATE items SET amount = amount - 1 WHERE item = '" + item + "'")
+        await pool.query("DELETE FROM cart WHERE item = '" + item + "'")
+        await pool.query("UPDATE items SET amount = amount - 1 WHERE item = '" + item + "'")
 
         }
     }
