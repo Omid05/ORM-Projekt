c1a57fe4270a8b64b5d64e1f70a273dd12b9b48c
marcuscheng0530 <marcus.cheng@elev.ga.ntig.se>
2025-05-12 10:41:36 +0200

    gjorde om min log in till ejs fil

--- log in/login.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#submitButton').addEventListener('click', function loginochanvändarnamn() {
-    const användarnamn = document.querySelector('#AnvändarnamnInput').value;
-    const lösenord = document.querySelector('#LösenordInput').value;
-    console.log(användarnamn + lösenord);
-  });
-  
